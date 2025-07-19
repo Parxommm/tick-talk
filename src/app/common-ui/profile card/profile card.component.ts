45a7ea2a6b103ee15ccd77cdf0601dd024ed6d23
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-07-19 23:13:35 +0300

    feat: create profile card component

---
+++ src/app/common-ui/profile card/profile card.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  imports: [],
+  templateUrl: './profile card.component.html',
+  styleUrl: './profile card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfileCardComponent {}
