4bf31d44d345861e053ebeb603e8db896b34e4b7
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-07-19 19:03:51 +0300

    init: app settings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile card/profile card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('tick-talk');
