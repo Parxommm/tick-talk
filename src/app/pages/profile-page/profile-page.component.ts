741f28a0a179b56edf24dcf6c2621081ba349236
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-08-27 17:48:12 +0300

    feat: create basic components for profile page

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -7,6 +7,7 @@ import { toObservable } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 import { SvgIconComponent } from '../../common-ui/svg-icon/svg-icon.component';
 import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+import { PostFeedComponent } from './post-feed/post-feed.component';
 
 @Component({
   selector: 'app-profile-page',
@@ -16,6 +17,7 @@ import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
     RouterLink,
     SvgIconComponent,
     ImgUrlPipe,
+    PostFeedComponent,
   ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
