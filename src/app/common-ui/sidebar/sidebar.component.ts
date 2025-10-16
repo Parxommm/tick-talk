530fa1edde5c46087f3db211e070c8ceb441d1df
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-10-17 00:02:27 +0300

    feat: implement search form

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ProfileService } from '../../data/services/profile.service';
 import { AsyncPipe } from '@angular/common';
 import { firstValueFrom } from 'rxjs';
-import { Profile } from '../../data/interfaces/profile.interface';
 import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
@@ -16,6 +15,7 @@ import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
     RouterLink,
     AsyncPipe,
     ImgUrlPipe,
+    RouterLinkActive,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
