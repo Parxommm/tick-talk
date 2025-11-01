5dbc1ee1d6f1ea60ebac147d52374f32ad786320
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-11-01 08:03:57 +0300

    feat: create basic components for chat page

---
+++ src/app/pages/chats-page/chats-btn/chats-btn.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chats-btn',
+  imports: [],
+  templateUrl: './chats-btn.component.html',
+  styleUrl: './chats-btn.component.scss',
+})
+export class ChatsBtnComponent { }
