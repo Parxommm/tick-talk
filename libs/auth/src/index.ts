45fdbb26dd6d6b449af16a84c4d54e01212fcdeb
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-11-15 16:00:16 +0300

    refactor: implement auth lib

---
+++ libs/auth/src/index.ts
@@ -0,0 +1,5 @@
+import { canActivateAuth } from './lib/auth/access.guard';
+import { authTokenInterceptor } from './lib/auth/auth.interceptor';
+import { AuthService } from './lib/auth/auth.service';
+
+export { canActivateAuth, authTokenInterceptor, AuthService };
