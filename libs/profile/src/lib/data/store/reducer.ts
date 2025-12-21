0da41602774db9111bdb15b6b4f0515b075c485a
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-12-21 11:29:42 +0300

    feat: implement infinite scroll for search page

--- libs/profile/src/lib/data/store/reducer.ts
+++ libs/profile/src/lib/data/store/reducer.ts
@@ -4,21 +4,45 @@ import { profileActions } from './actions';
 
 export interface ProfileState {
   profilers: Profile[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   profileFilters: Record<string, any>;
+  page: number;
+  size: number;
 }
 
 export const initialProfileState: ProfileState = {
   profilers: [],
   profileFilters: {},
+  page: 1,
+  size: 10,
 };
 
 export const profileFeature = createFeature({
   name: 'profileFeature',
   reducer: createReducer(
     initialProfileState,
+
     on(profileActions.profilesLoaded, (state, payload) => ({
       ...state,
-      profilers: payload.profiles,
-    }))
+      profilers: state.profilers.concat(payload.profiles),
+    })),
+
+    on(profileActions.filterEvents, (state, payload) => ({
+      ...state,
+      profilers: [],
+      profileFilters: payload.filters,
+      page: 1,
+    })),
+
+    on(profileActions.setPage, (state, payload) => {
+      let page = payload.page;
+
+      if (!page) page = state.page + 1;
+
+      return {
+        ...state,
+        page,
+      };
+    })
   ),
 });
--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -1,16 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { ProfileCardComponent } from '../../ui';
 import { ProfileFiltersComponent } from '../profile-filters/profile-filters.component';
-import { selectFilteredProfiles } from '../../data';
+import { profileActions, selectFilteredProfiles } from '../../data';
 import { Store } from '@ngrx/store';
+import { InfiniteScrollTriggerComponent } from '@tt/common-ui';
 
 @Component({
   selector: 'tt-search-page',
-  imports: [ProfileCardComponent, ProfileFiltersComponent],
+  imports: [
+    ProfileCardComponent,
+    ProfileFiltersComponent,
+    InfiniteScrollTriggerComponent,
+  ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
 export class SearchPageComponent {
   store = inject(Store);
   profiles = this.store.selectSignal(selectFilteredProfiles);
+
+  timeToFetch() {
+    this.store.dispatch(profileActions.setPage({}));
+  }
 }
