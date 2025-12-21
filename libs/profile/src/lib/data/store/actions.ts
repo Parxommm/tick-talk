9f6c6da99052e604096cf3d80d727c16f66ff637
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-12-21 11:29:42 +0300

    feat: implement infinite scroll for search page

--- libs/profile/src/lib/data/store/actions.ts
+++ libs/profile/src/lib/data/store/actions.ts
@@ -4,7 +4,9 @@ import { Profile } from '@tt/interfaces/profile';
 export const profileActions = createActionGroup({
   source: 'profile',
   events: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     'filter events': props<{ filters: Record<string, any> }>(),
     'profiles loaded': props<{ profiles: Profile[] }>(),
+    'set page': props<{ page?: number }>(),
   },
 });
