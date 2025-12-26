5fd635193eff77dfc62f1fdeed35a7d12893eaeb
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2025-12-26 18:40:09 +0300

    feat: implement burger-menu

--- libs/layout/src/lib/layout/layout.component.ts
+++ libs/layout/src/lib/layout/layout.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { SvgIconComponent } from '@tt/common-ui';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'tt-layout',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, SvgIconComponent, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  isSidebarOpen = signal<boolean>(window.innerWidth > 768);
+
+  constructor() {
+    // Слушаем событие закрытия меню из sidebar
+    window.addEventListener('closeSidebar', () => {
+      if (window.innerWidth <= 768) {
+        this.closeSidebar();
+      }
+    });
+
+    // Обновляем состояние при изменении размера окна
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 768) {
+        this.isSidebarOpen.set(true);
+      } else if (window.innerWidth <= 768 && this.isSidebarOpen()) {
+        // Закрываем меню при переходе с десктопа на мобильный
+        this.closeSidebar();
+      }
+    });
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen.set(!this.isSidebarOpen());
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen.set(false);
+  }
+}
